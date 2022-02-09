332ddb0f16c536c2c40596a3654cd987ef05f4a8
Leon Armstrong <leoncito@live.com>
2022-02-09 21:17:00 +0000

    For Loop
    Pet, Car model and Holiday, For print character e.g. "P"

---
+++ ForLoop.py
@@ -0,0 +1,19 @@
+parrot = "Norwegian Blue"
+
+for character in parrot:  # character: 'N'
+    print(character)
+
+dog = "Bull Terrier"
+
+for character in dog:  # character: 'D'
+    print(character)
+
+car = "Bmw M3 E36 GT2"  # character: 'C'
+
+for character in car:
+    print(character)
+
+ship_holiday = "Royal West indian"  # character: 'R'
+
+for character in ship_holiday:
+    print(character)
