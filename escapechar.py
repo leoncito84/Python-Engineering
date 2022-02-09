e02a0d08e441f5106d95299bdc4cf22b452ca173
Leon Armstrong <leoncito@live.com>
2022-02-09 21:26:12 +0000

    The Escape Character
    This is very difficult this word of letters are called splitSring

---
+++ escapechar.py
@@ -0,0 +1,16 @@
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
+# or
+print("The pet shop owner said \"No, no 'e's uh,...he's resting\".")
+
+print("""The pet shop owner said "No, no, 
+'e's uh, ....he's resting".""")
+
+anotherSplitString = """This string has been 
+split over\
+several\
+lines"""
+
+print(anotherSplitString)
