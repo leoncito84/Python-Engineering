0e8d55b9968084f15ca5b3a3fc2269b153299843
Leon Armstrong <leoncito@live.com>
2022-02-10 19:35:47 +0000

    List - Computer parts

---
+++ Lists.py
@@ -0,0 +1,16 @@
+computer_parts = ["computer",
+                  "monitor",
+                  "keyboard",
+                  "mouse",
+                  "mouse mat"
+                  ]
+
+for part in computer_parts:
+    print(print)
+
+print()
+print(computer_parts[0])
+
+print()
+print(computer_parts[0:3])
+print(computer_parts[-1])
