d6d62a2a1b3dc9bdd4c592355967184534326a90
Leon Armstrong <leoncito@live.com>
2022-02-15 23:07:15 +0000

    Day, Temperature and Raining

---
+++ True.py
@@ -0,0 +1,17 @@
+day = "Saturday"
+temperature = 30
+raining = True
+
+if day == "Saturday" and temperature > 27 or not raining:
+    print("Go swimming")
+else:
+    print("Learn Python haha")
+
+day = "Friday"
+temperature = 30
+raining = False
+
+if day == "Saturday" and temperature > 27 or not raining:
+    print("Go swimming")
+else:
+    print("Learn Python haha")
