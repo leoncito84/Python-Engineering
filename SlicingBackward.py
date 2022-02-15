7cac654bcec57bb77489ff952f1262b183e3faaf
Leon Armstrong <leoncito@live.com>
2022-02-15 22:55:54 +0000

    SlicingBackward
    Large words of letters, backwards of numeracy and print in backwards

---
+++ SlicingBackward.py
@@ -0,0 +1,27 @@
+letters = "abcefghijklmnopqrstuvwxyz"
+backwards = letters[25:0:-1]
+print(backwards)
+
+letters = "abcefghijklmnopqrstuvwxyz"
+backwards = letters[16:0:-1]
+print(backwards)
+
+letters = "abcefghijklmnopqrstuvwxyz"
+backwards = letters[11:0:-1]
+print(backwards)
+
+letters = "abcefghijklmnopqrstuvwxyz"
+backwards = letters[22:0:-1]
+print(backwards)
+
+letters = "abcefghijklmnopqrstuvwxyz"
+backwards = letters[5:0:-1]
+print(backwards)
+
+letters = "abcefghijklmnopqrstuvwxyz"
+backwards = letters[1:0:-1]
+print(backwards)
+
+letters = "abcefghijklmnopqrstuvwxyz"
+backwards = letters[14:0:-1]
+print(backwards)
