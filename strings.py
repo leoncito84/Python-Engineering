2a4c30dffe60192cdc8a2495e3fc0dc3aa96b4b8
Leon Armstrong <leoncito@live.com>
2022-02-15 23:00:39 +0000

    string part 1
    Short Hobbies

---
+++ strings.py
@@ -0,0 +1,17 @@
+print("Today is good day to learn Python")
+print('Python is fun')
+print("Python's string are easy to use")
+print('We can even include "quotes" in string')
+print("Hello" + " world")
+greeting = "Hello"
+name = "Leon"
+print(greeting + name)
+
+# if we want a space, we can add that too
+print(greeting + ' ' + name)
+
+age = 36
+print(age)
+
+print(type(greeting))
+print(type(age))
