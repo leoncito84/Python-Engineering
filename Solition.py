bbe50a536b70e50590c215d4dd35578145fb37ae
Leon Armstrong <leoncito@live.com>
2022-02-15 22:58:10 +0000

    Staff is requires customer's account or ID proof

---
+++ Solition.py
@@ -0,0 +1,8 @@
+name = input("Please enter your name: ")
+age = int(input("How old are you"))
+if 18 <= age < 31:
+    print("Welcome club 18-30 holidays, {0}".format(name))
+
+else:
+    print("Sorry I am afraid that's not allow for under 18, our holiday are only for cool people")
+print
